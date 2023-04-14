c3600ee59761e78de2993354ac8df02645e44363
Santiago Aragon <santiago.aragon@oracle.com>
2023-04-14 12:49:39 -0600

    Initial Commit

---
+++ CondoConnect/CondoConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CondoConnectApp.swift
+//  CondoConnect
+//
+//  Created by Santiago Aragon on 14/04/23.
+//
+
+import SwiftUI
+
+@main
+struct CondoConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CondoConnect/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CondoConnect
+//
+//  Created by Santiago Aragon on 14/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
